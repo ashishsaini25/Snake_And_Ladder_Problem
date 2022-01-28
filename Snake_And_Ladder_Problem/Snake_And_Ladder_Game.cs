772adccaed2d1ddd21dba8cad96c74c00ce98044
ghost331 <67087070+ghost331@users.noreply.github.com>
2022-01-29 00:30:00 +0530

    [Ashish] Add: started game with player position 0

---
+++ Snake_And_Ladder_Problem/Snake_And_Ladder_Game.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake_And_Ladder_Problem
+{
+    internal class Snake_And_Ladder_Game
+    {
+    }
+}
